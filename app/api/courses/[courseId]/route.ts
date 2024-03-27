6f5a9e9b116a0ed96f646d70c9c3845c118310ee
NouhaSejil <160036551+NouhaSejil@users.noreply.github.com>
2024-03-27 23:49:38 +0100

    Update route.ts

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
-import { isTeacher } from "@/lib/teacher";
+
 
 const mux = new Mux({
   tokenId: process.env.MUX_TOKEN_ID,
@@ -17,7 +17,7 @@ export async function DELETE(
   try {
     const { userId } = auth();
 
-    if (!userId||!isTeacher(userId)) {
+    if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
@@ -67,7 +67,7 @@ export async function PATCH(
     const { courseId } = params;
     const values = await req.json();
 
-    if (!userId||!isTeacher(userId)) {
+    if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
